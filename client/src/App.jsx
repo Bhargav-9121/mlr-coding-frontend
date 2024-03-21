584f786aa1c61b27cc339fb9983ee25f5ae39323
P ABHIJIT <21r21a05p4@mlrinstitutions.ac.in>
2024-03-21 18:58:12 +0530

    fetched

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,26 @@
 import DataTable from "./components/DataTable";
+import api from "./api/axiosConfig";
+import { useState, useEffect } from "react";
 
-const App = () => <DataTable />;
+const App = () => {
+
+    const [data, setData] = useState([]);
+
+    const getData = async () => {
+        try {
+            const response = await api.get("/leaderboard");
+            const sortedData = response.data.sort((a, b) => b.total - a.total); // Sort data by totalScore in descending order
+            setData(sortedData);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    return <DataTable data={data} />;
+};
 
 export default App;
