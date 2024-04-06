d3dba6f8723a740de214a0408f12cdb617d9c096
P ABHIJIT <21r21a05p4@mlrinstitutions.ac.in>
2024-04-06 10:00:10 +0530

    Heatmap

--- client/src/components/LoginPage/index.jsx
+++ client/src/components/LoginPage/index.jsx
@@ -1,71 +1,59 @@
-import React from 'react';
-import LottieAnimation from '../../components/LottieAnimation'; // Import the component
-import {MDBContainer, MDBCol, MDBRow, MDBBtn, MDBIcon, MDBInput, MDBCheckbox, MDBCard, MDBCardBody } from 'mdb-react-ui-kit';
+import React, { useState, useEffect } from 'react';
+import LottieAnimation from '../../components/LottieAnimation';
+import { MDBContainer, MDBCol, MDBRow, MDBBtn, MDBIcon, MDBInput, MDBCheckbox, MDBCard, MDBCardBody, MDBSpinner } from 'mdb-react-ui-kit';
 
 function LoginPage() {
+    const [loading, setLoading] = useState(true); // State variable to track loading status
     const animationSrc = 'https://lottie.host/d01a53d2-5dfb-49be-8dd4-5da9bc887cd2/9qciwN240m.json'; 
-  return (
 
-    //animation div
-     <div style={{display:'flex', justifyContent:'center'}}>
+    useEffect(() => {
+        // Simulate loading delay with setTimeout
+        setTimeout(() => {
+            setLoading(false); // Set loading to false after a delay
+        }, 2000); // 2000 milliseconds (2 seconds) delay
+    }, []);
 
-      <MDBCard style={{marginTop: '8%', width: '95%'}}>
-      <MDBCardBody style={{padding: '0'}}>
-    <MDBContainer fluid className=" h-custom">
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+            {loading ? (
+                // Render loading spinner while loading is true
+                <MDBSpinner grow className="m-5" />
+            ) : (
+                // Render the page content when loading is false
+                <MDBCard style={{ marginTop: '8%', width: '95%', border: 'solid purple 2px' }}>
+                    <MDBCardBody style={{ padding: '0' }}>
+                        <MDBContainer fluid className=" h-custom">
+                            <MDBRow>
+                                <MDBCol col='10' md='6' style={{ borderRight: '1px solid black' }}>
+                                    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'white', paddingBottom: '10%' }}>
+                                        <LottieAnimation
+                                            src={animationSrc}
+                                            loop={true}
+                                            width={500}
+                                            height={500}
+                                            autoplay={true}
+                                        />
+                                    </div>
+                                </MDBCol>
 
-      <MDBRow>
-
-        <MDBCol col='10' md='6' style={{borderRight: '1px solid black'}}>
-        <div style={{display:'flex', justifyContent:'center', alignItems: 'center', backgroundColor: 'white', paddingBottom: '10%'}}>
-        <LottieAnimation
-            src={animationSrc}
-            loop={true} 
-            width={500}
-            height={500}
-            autoplay={true}
-        />
+                                {/* login card */}
+                                <MDBCol col='4' md='6' style={{ backgroundColor: '#F5E1FF' }} >
+                                    <div style={{ padding: '5%' }}>
+                                        <h2 className="fw-bold mb-5 mt-5 text-center">Sign in</h2>
+                                        <MDBInput wrapperClass='mb-4 w-70' label='Username' id='formControlLg' type='text' size="lg" style={{ backgroundColor: 'white' }} />
+                                        <MDBInput wrapperClass='mb-4 w-70' label='Password' id='formControlLg' type='password' size="lg" style={{ backgroundColor: 'white' }} />
+                                        <MDBBtn>Login</MDBBtn>
+                                        <hr className="my-4" />
+                                        <p className="small mb-3 pb-lg-2"><a class="text-black-50" href="#!">Forgot password?</a></p>
+                                    </div>
+                                </MDBCol>
+                            </MDBRow>
+                        </MDBContainer>
+                    </MDBCardBody>
+                </MDBCard>
+            )}
         </div>
-        
-        </MDBCol>
-        
-
-    {/* login card */}
-        <MDBCol col='4' md='6' style={{backgroundColor:'#F5E1FF'}} >
-            <div style={{padding: '5%'}}>
-
-      
-
-              <h2 className="fw-bold mb-5 mt-5 text-center">Sign in</h2>
-
-              <MDBInput wrapperClass='mb-4 w-70' label='Username' id='formControlLg' type='text' size="lg" style={{backgroundColor:'white'}}/>
-              <MDBInput wrapperClass='mb-4 w-70' label='Password' id='formControlLg' type='password' size="lg"  style={{backgroundColor:'white'}}/>
-
-
-              <MDBBtn>
-                Login
-              </MDBBtn>
-
-              <hr className="my-4" />
-
-              <p className="small mb-3 pb-lg-2"><a class="text-black-50" href="#!">Forgot password?</a></p>
-             
-
-        
-              </div>
-
-        </MDBCol>
-
-      </MDBRow>
-
-      
-
-    </MDBContainer>
-    </MDBCardBody>
-    </MDBCard>
-
-    </div>
-  );
+    );
 }
 
 export default LoginPage;
-
--- client/src/components/Profile/index.jsx
+++ client/src/components/Profile/index.jsx
@@ -11,13 +11,12 @@ import {
   MDBCardText,
   MDBCardBody,
   MDBCardImage,
-  MDBProgress,
-  MDBProgressBar,
   MDBIcon,
   MDBListGroup,
   MDBListGroupItem,
 } from "mdb-react-ui-kit";
 import DashBoard from "../DashBoard";
+import HeatMap from "../HeatMap";
 
 export default function ProfilePage({stats}) {
   if (!stats) {
@@ -154,62 +153,19 @@ export default function ProfilePage({stats}) {
               </MDBCard>
             </MDBRow>
             <MDBRow>
-              <MDBCard className="mb-4 mb-md-0">
-                <MDBCardBody>
-                  <MDBCardText className="mb-4">
-                    <span className="text-primary font-italic me-1">
-                      assigment
-                    </span>{" "}
-                    Project Status
-                  </MDBCardText>
-                  <MDBCardText className="mb-1" style={{ fontSize: ".77rem" }}>
-                    Web Design
-                  </MDBCardText>
-                  <MDBProgress className="rounded">
-                    <MDBProgressBar width={80} valuemin={0} valuemax={100} />
-                  </MDBProgress>
-
-                  <MDBCardText
-                    className="mt-4 mb-1"
-                    style={{ fontSize: ".77rem" }}
-                  >
-                    Website Markup
-                  </MDBCardText>
-                  <MDBProgress className="rounded">
-                    <MDBProgressBar width={72} valuemin={0} valuemax={100} />
-                  </MDBProgress>
-
-                  <MDBCardText
-                    className="mt-4 mb-1"
-                    style={{ fontSize: ".77rem" }}
-                  >
-                    One Page
-                  </MDBCardText>
-                  <MDBProgress className="rounded">
-                    <MDBProgressBar width={89} valuemin={0} valuemax={100} />
-                  </MDBProgress>
-
-                  <MDBCardText
-                    className="mt-4 mb-1"
-                    style={{ fontSize: ".77rem" }}
-                  >
-                    Mobile Template
-                  </MDBCardText>
-                  <MDBProgress className="rounded">
-                    <MDBProgressBar width={55} valuemin={0} valuemax={100} />
-                  </MDBProgress>
-
-                  <MDBCardText
-                    className="mt-4 mb-1"
-                    style={{ fontSize: ".77rem" }}
-                  >
-                    Backend API
-                  </MDBCardText>
-                  <MDBProgress className="rounded">
-                    <MDBProgressBar width={66} valuemin={0} valuemax={100} />
-                  </MDBProgress>
-                </MDBCardBody>
+              <MDBCard className="mb-4 mb-md-0" style={{padding: '3rem'}}>
+                <div style={{display: 'flex' , justifyContent: 'space-evenly', alignItems: 'end'}}>
+                  <div>
+                    Total Active Days: 4
+                  </div>
+                  <div>
+                    Max Streak : 2
+                  </div>
+                </div>
+              <HeatMap style={{width: '10rem'}}/>
+                
               </MDBCard>
+              
             </MDBRow>
           </MDBCol>
         </MDBRow>
